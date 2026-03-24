b57b8bc589ea106ff48c034b788281f063967154
Rithika Patha <rithika.p@brandwar.in>
2026-03-24 11:28:40 +0530

    removed unwanted files

--- list_admins.js
+++
@@ -1,6 +0,0 @@
-
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-p.admin.findMany({ where: { companyId: '4bfd807c-7ad8-457b-bee5-9d39dc10e980' } })
-    .then(a => console.log(JSON.stringify(a, null, 2)))
-    .finally(() => p.$disconnect());
--- list_roles.js
+++
@@ -1,6 +0,0 @@
-
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-p.role.findMany({ where: { companyId: '4bfd807c-7ad8-457b-bee5-9d39dc10e980' } })
-    .then(r => console.log(JSON.stringify(r, null, 2)))
-    .finally(() => p.$disconnect());
